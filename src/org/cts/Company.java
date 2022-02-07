382e94d40b8504bea611e9470dce08375585d0ba
raghavanravi6 <raghavanravi662k@gmail.com>
2022-02-07 08:49:50 +0530

    Work Done

--- src/org/cts/Company.java
+++ src/org/cts/Company.java
@@ -1,5 +1,17 @@
 package org.cts;
 
 public class Company {
+	public void compName() {
+		System.out.println("Company name is Greenstech");
+
+	}
+	public void compid() {
+		System.out.println("company id is A2022B06");
+
+	} public static void main(String[] args) {
+		Company c=new Company();
+		c.compid();
+		c.compName();
+	}
 
 }
